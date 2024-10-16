49fcd8a3483f432a171a78833d743a455f6aa587
Victor Omoniyi <vomoniyi2001@gmail.com>
2024-10-16 15:21:58 +0100

    Project Setup - Folder Structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as firebaseAdmin from 'firebase-admin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  // Initialize Firebase Admin
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.applicationDefault(),
+    databaseURL: 'https.firebaseio.com',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
